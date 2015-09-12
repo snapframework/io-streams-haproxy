snapframework/io-streams-haproxy

commit f24da8a7c7722e5e1fd7d8b890db6361a28b35b3

Use the Num instance instead of PortNum constructor.
The latter form is deprecated. Num instance for PortNumber has been available
since at least network-2.3 (lower bound of this package).

src/System/IO/Streams/Network/HAProxy.hs
@@ -281,8 +281,8 @@ parseNewHaProxy localProxyInfo = do
         void $ take $ fromIntegral nskip
 
         -- Note: we actually want the brain-dead constructors here
-        let sa = N.SockAddrInet (N.PortNum srcPort) srcAddr
-        let sb = N.SockAddrInet (N.PortNum destPort) destAddr
+        let sa = N.SockAddrInet (fromIntegral srcPort) srcAddr
+        let sb = N.SockAddrInet (fromIntegral destPort) destAddr
         return $! makeProxyInfo sa sb (addrFamily sa) socketType
 
     handleIPv6 addressLen socketType = do
@@ -308,8 +308,8 @@ parseNewHaProxy localProxyInfo = do
 
         void $ take $ fromIntegral nskip
 
-        let sa = N.SockAddrInet6 (N.PortNum sp) flow (s1, s2, s3, s4) scopeId
-        let sb = N.SockAddrInet6 (N.PortNum dp) flow (d1, d2, d3, d4) scopeId
+        let sa = N.SockAddrInet6 (fromIntegral sp) flow (s1, s2, s3, s4) scopeId
+        let sb = N.SockAddrInet6 (fromIntegral dp) flow (d1, d2, d3, d4) scopeId
 
         return $! makeProxyInfo sa sb (addrFamily sa) socketType
 #ifndef WINDOWS

commit ab208d49fe73c9cf05e46c0d929eec311fb0cfd9

Fix testsuite failure caused by f24da8a7c7722e5e1fd7d8b890db6361a28b35b3.

src/System/IO/Streams/Network/HAProxy.hs
@@ -280,8 +280,8 @@ parseNewHaProxy localProxyInfo = do
         let nskip = addressLen - 12
         srcAddr  <- snarf32
         destAddr <- snarf32
-        srcPort  <- snarf16
-        destPort <- snarf16
+        srcPort  <- ntohs <$> snarf16
+        destPort <- ntohs <$> snarf16
         void $ take $ fromIntegral nskip
 
         -- Note: we actually want the brain-dead constructors here
@@ -307,8 +307,8 @@ parseNewHaProxy localProxyInfo = do
         d3 <- ntohl <$> snarf32
         d4 <- ntohl <$> snarf32
 
-        sp <- snarf16
-        dp <- snarf16
+        sp <- ntohs <$> snarf16
+        dp <- ntohs <$> snarf16
 
         void $ take $ fromIntegral nskip
 
@@ -355,9 +355,7 @@ addrFamily :: N.SockAddr -> N.Family
 addrFamily s = case s of
                  (N.SockAddrInet _ _)      -> N.AF_INET
                  (N.SockAddrInet6 _ _ _ _) -> N.AF_INET6
-#ifdef WINDOWS
-                 _                         -> error "unknown family"
-#else
+#ifndef WINDOWS
                  (N.SockAddrUnix _ )       -> N.AF_UNIX
-                 _                         -> error "unknown family"
 #endif
+                 _                         -> error "unknown family"